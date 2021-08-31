9d096f7e7cd61ec9050274c3393d33af3c567cb0
John Brimley <johnbrimley@gmail.com>
2021-08-31 17:55:10 -0600

    Added app component, material, and routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from './material.module'
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        CommonModule,
+        AppRoutingModule,
+        MaterialModule,
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
